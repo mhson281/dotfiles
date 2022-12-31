cbe214c3a4768e763d78964a19c1bd88a4bf8b77
Minh Son <54775734+mhson281@users.noreply.github.com>
2022-12-31 13:57:49 -0600

    adding script to install zsh plugins

---
+++ zsh_plugins_install.sh
@@ -0,0 +1,5 @@
+#! /bin/zsh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/agkozak/zsh-z ~/.oh-my-zsh/custom/plugins/zsh-z
