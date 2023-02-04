b941380cd60e9ec79d74cca565fd61a59841660b
Minh Son <54775734+mhson281@users.noreply.github.com>
2023-02-04 15:54:26 -0600

    created new script to install packages and plugins

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-#! /bin/zsh
+#!/bin/bash
+
+# installing most used packages
+brew update && brew upgrade
+brew install git
+brew install lsd
+brew cask install iterm2
+
+#installing zsh plugins
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
